b580706f5e56f7e08a04dab8d7d4f83fbf1d128c
01000101 <jcornutt@gmail.com>
2016-10-29 18:18:09 -0400

    Fixing examples

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from aria.consumption import (
     Read, Validate, Model, Instance)
 from aria.loading import LiteralLocation
 
-from nfvo_packager.writer import CSARWriter
+from nfvo_packager.writer import CSARReader, CSARWriter
 
 install_aria_extensions()
 
@@ -30,13 +30,8 @@ def parse_text(payload, search_paths=None):
     return None
 
 
-def main():
-    '''Entry point'''
-    build = CSARWriter('examples/csar_hello_world_nometa',
-                       entry='definitions/tosca_elk.yaml',
-                       author='Gigaspaces',
-                       output='examples/csar_hello_world_nometa.zip')
-    csar = build.reader
+def dump_info(csar):
+    '''Dumps information from a CSAR reader'''
     print 'Path: %s' % csar.path
     print 'Author: %s' % csar.author
     print 'Version: %s' % csar.version
@@ -50,15 +45,34 @@ def main():
         [os.path.join(csar.path, 'definitions')])
     # Dump information about the final blueprint
     for _, node in aria_res.nodes.items():
-        print 'Node: %s:' % node.id
+        print '\nNode: %s:' % node.id
         print '| type: %s' % node.type_name
         print '| template: %s' % node.template_name
-        print '| properties: %s' % node.properties
+        print '| properties:'
+        for pname, prop in node.properties.items():
+            print '  | %s: %s' % (pname, prop.value)
         print '| capabilities:'
         for _, capability in node.capabilities.items():
             print '  | %s:' % capability.name
             for pname, prop in capability.properties.items():
                 print '    | %s: %s' % (pname, prop.value)
+
+
+def main():
+    '''Entry point'''
+    print '\n\n======================='
+    print '== VNF CHAINING CSAR =='
+    print '======================='
+    build = CSARWriter('examples/csar_vnf_chaining',
+                       entry='service.yaml',
+                       author='Gigaspaces',
+                       output='examples/csar_vnf_chaining.zip')
+    dump_info(build.reader)
+    
+    print '\n\n==================================='
+    print '== HELLO WORLD CSAR W/ ARTIFACTS =='
+    print '==================================='
+    dump_info(CSARReader('examples/csar_hello_world.zip'))
 
 
 main()
